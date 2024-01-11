57645c4ce17b90fed8be803c694a893299171dc9
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-01-11 22:28:07 +0100

    Add new pages and components

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,11 @@
+import "./style.scss"
+
+function Footer() {
+  return (
+    <footer>
+      <p className="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
+  );
+}
+
+export default Footer;
