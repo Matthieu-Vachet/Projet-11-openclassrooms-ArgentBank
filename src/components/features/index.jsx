4ed4c5ecfc836616ec540e5b7c426930d4189900
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-01-12 12:46:51 +0100

    Add features section and icons

---
+++ src/components/features/index.jsx
@@ -0,0 +1,15 @@
+import "./style.scss"
+
+function Features(children) {
+
+
+    return (
+        <div className="feature-item">
+            <img className="feature-icon" src={children.image} alt={children.alt} />
+            <h3 className="feature-item-title">{children.title}</h3>
+            <p>{children.description}</p>
+        </div>
+    )
+}
+
+export default Features
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,12 +1,56 @@
 import Hero from "../../components/hero";
+import Features from "../../components/features";
+
+import iconChat from "../../assets/icon-chat.png";
+import iconMoney from "../../assets/icon-money.png";
+import iconSecurity from "../../assets/icon-security.png";
 
 import "./style.scss";
 
+
 function Home() {
+
+  // Tableau des features
+  const features = [
+    {
+      image: iconChat,
+      alt : "Icon Chat",
+      title : `You are our #1 priority`,
+      description : `Need to talk to a representative? You can get in touch through our 
+      24/7 chat or through a phone call in less than 5 minutes.`
+    },
+    {
+      image: iconMoney,
+      alt : "Icon Money",
+      title : `More savings means higher rates`,
+      description : `The more you save with us, the higher your interest rate will be!`
+    },
+    {
+      image: iconSecurity,
+      alt : "Icon Security",
+      title : `Security you can trust`,
+      description : `We use top of the line encryption to make sure your data and money 
+      is always safe.`
+    }
+  ]
+
+
   return (
-    <div>
+    <main>
       <Hero />
-    </div>
+      <section className="features">
+        <h2 className="sr-only">Features</h2>
+        {features.map((e, index) => (
+          <Features
+            image={e.image}
+            alt={e.alt}
+            title={e.title}
+            description={e.description}
+            key={index}
+          />
+        ))}
+      </section>
+    </main>
   );
 }
 
