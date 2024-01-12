39852708f7b3b6317b418c1977a29e437e30fa57
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-01-12 13:40:59 +0100

    Add email and password state and onSubmit function to Form component

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,19 +1,34 @@
+import { useState } from "react";
 import "./style.scss";
 
 function Form() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    async function onSubmit(event) {
+        event.preventDefault();
+
+        console.log("State email :", email);
+        console.log("State password :", password);
+    }
+
+
     return (
-        <form>
+        <form onSubmit={onSubmit}>
             <div className="input-wrapper">
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username" />
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" 
+                onChange={(e) => setEmail(e.target.value)} required/>
             </div>
             <div className="input-wrapper">
                 <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
+                <input type="password" id="password" 
+                onChange={(e) => setPassword(e.target.value)} required/>
             </div>
             <div className="input-remember">
-                <label htmlFor="remember-me">Remember me</label>
                 <input type="checkbox" id="remember-me" />
+                <label htmlFor="remember-me">Remember me</label>
             </div>
             <button className="sign-in-button">Sign in</button>
         </form>
