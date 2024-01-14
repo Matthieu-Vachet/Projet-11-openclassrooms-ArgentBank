0625ef659b88b03475b62e25b1b77439e1a117b7
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-01-14 16:20:25 +0100

    Refactor header component and add logo component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,21 +1,18 @@
 import { Link } from "react-router-dom";
-
-import Logo from "../../assets/argentBankLogo.png";
+import Logo from "../logo";
 
 import "./style.scss"
+
 
 function Header() {
   return (
     <header>
       <nav className="main-nav">
-        <Link to="/" className="main-nav-logo">
-          <img src={Logo} alt="Argent Bank Logo" className="main-nav-logo-image" />
-          <h1 className="sr-only">Argent Bank</h1>
-        </Link>
+        <Logo />
         <Link to="/login" className="main-nav-item">
-                <i className="fa fa-user-circle"></i>
-                    Sign In 
-            </Link>
+          <i className="fa fa-user-circle"></i>
+          Sign In
+        </Link>
       </nav>
     </header>
   );
---
+++ src/components/logo/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+import LogoImg from "../../assets/argentBankLogo.png";
+import "./style.scss";
+
+function Logo() {
+  return (
+    <Link to="/" className="main-nav-logo">
+          <img src={LogoImg} alt="Argent Bank Logo" className="main-nav-logo-image" />
+          <h1 className="sr-only">Argent Bank</h1>
+        </Link>
+  )
+}
+
+export default Logo;
