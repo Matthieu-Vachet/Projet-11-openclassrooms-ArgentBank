f3a8f9ea532ea5f17b68c7201c4e9bc92638af65
Matthieu-V <79032023+Matthieu-Vachet@users.noreply.github.com>
2024-01-14 15:11:12 +0100

    Add UserEdit component with styling

---
+++ src/components/userEdit/index.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import "./style.scss"
+
+function UserEdit () {
+    
+    // A modif pour Redux
+    const { edit, setEdit } = useState(false);
+    const userName = "Tony Jarvis";
+    console.log(edit);
+
+  return (
+    <div className="header">
+      <h1>Welcom back<br />{userName}!</h1>
+      <button className="edit-button" onClick={() => setEdit(true)}>Edit Name</button>
+    </div>
+  )
+}
+
+export default UserEdit
